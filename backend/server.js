81d45ebb65b31283e351b0115b758aec0a12f334
Kevin Wang <kcwang06@gmail.com>
2020-02-10 18:12:27 -0500

    setting up graphql structure

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+const graphqlHTTP = require('express-graphql');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const {buildSchema} = require('graphql');
 
 require('dotenv').config();
 
@@ -17,8 +18,24 @@ connection.once('open',() =>{console.log("MongoDB database connection establishe
 app.use(cors());
 app.use(bodyParser.json());
 
+// allow cross origin requests
+app.use('/graphql', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,  Authorization, Content-Length, X-Requested-With');
+  if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+  } else {
+      next();
+  }
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
 const usersRouter = require('./routes/users');
-const concertsRouter = require(./routes/concerts');
+const concertsRouter = require('./routes/concerts');
 
 app.use('/users', usersRouter);
 app.use('./concerts', concertsRouter);
--- server/app.js
+++ server/app.js
@@ -22,6 +22,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('/graphql', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,  Authorization, Content-Length, X-Requested-With');
+  if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+  } else {
+      next();
+  }
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
